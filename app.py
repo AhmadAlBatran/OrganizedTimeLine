0b49cbe985b8d790fe7955271144f3f9cded4f93
AhmadAlBatran <1AhmadBatran@gmail.com>
2026-04-23 15:35:22 +0300

    test deployment

--- app.py
+++ app.py
@@ -1,12 +1,38 @@
-from flask import Flask, render_template
+import os
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
 
 @app.route("/")
-def index():
-    return render_template("index.html")
+def hello():
+    return "Hello World"
+
+
+@app.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    user = User(username=data["username"], password=data["password"])
+    db.session.add(user)
+    db.session.commit()
+    return jsonify({"message": "User created"}), 201
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
